60635c06c88fb985c6ede66414f618e2031b3753
Your Name <your_email@example.com>
2023-05-30 13:41:39 +0300

    v6

--- app.js
+++ app.js
@@ -4,30 +4,42 @@ var app = express();
 var path = require('path');
 var ejs = require("ejs");
 var items = [];
+let calısmaItem = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", function (req, res) {
-    var today = new Date();
+    let today = new Date();
    
        
-   var options = {
+   let options = {
     weekday : "long",
     day: "numeric",
     month: "long"
    };
 
-   var day = today.toLocaleDateString("tr-TR", options);
+   let day = today.toLocaleDateString("tr-TR", options);
         
-        res.render("list", { gunTuru: day, newlistItems: items });
+   res.render("list", { gunTuru: day, newlistItems: items });
     
 
 });
 app.post("/" , function (req, res) {
-   var item =req.body.newItem;
-   items.push(item);
-    res.redirect("/")
+    var item =req.body.newItem;
+    if (req.body.list === "work"){
+        calısmaItem.push(item)
+        res.redirect("/work")
+    }else {
+        items.push(item);
+        res.redirect("/") 
+    }
+   
+   
+})
+
+app.get("/work" ,function (req, res) {
+    res.render("list",{gunTuru:"calışma list", newlistItems: calısmaItem})
 })
 
 
